0d8170cc790853a82d4906da1d64fc35427fa461
Ani Kenechi Kennedy <kennedyani.main@gmail.com>
2025-09-08 21:27:03 +0200

    Added commit

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -80,31 +80,31 @@ set_architectura_interior_design, architectura_interior_design, valueRicerca, se
             {/** Hero section */}
             <FadeIn>
             <div  style={{background: `url(${herox})`,
-                backgroundSize: `cover`, backgroundPosition: `center`}} className=" min-h-screen img bg-cover flex-col items-center bg-center  text-white md:px-5 py-2">
+                backgroundSize: `cover`, backgroundPosition: `center`}} className=" min-h-screen img bg-cover flex-col items-center bg-center  text-white md:px-5 ">
                     
                 {/* Contact Bar */}
-                <div className="flex w-full justify-between items-center text-sm px-2 py-1">
+                <div className="flex flex-col sm:flex-row  w-full justify-between items-center text-sm px-2 py-1">
                     {/* phone and email */}
                     
-                    <div className="flex flex-col md:flex-row -ml-0 lg:flex-row w-10 md:w-1/2  justify-start md:items-center">
+                    <div className="flex flex-col md:flex-row w-full lg:flex-row md:w-1/2  justify-start md:items-center">
                         <FaPhone className="md:w-1/5  hidden md:inline"/>
-                        <span className="md:mr-2 w-5 ml-[0rem] md:w-full text-[0.9rem]">+393311887849 | 0573-737305</span>
+                        <span className="md:mr-2 ml-[0rem] md:w-full text-[0.9rem] w-full">+393311887849 | 0573-737305</span>
 
                         <FaEnvelope className=" md:w-1/5  hidden md:inline"/>
-                        <span className=" text-[0.9rem] ">gunzimangusta@gmail.com</span>
+                        <span className=" text-[0.9rem] w-1/2">gunzimangusta@gmail.com</span>
                     </div>
                     
 
                     {/* time */}
-                    <div className="flex flex-col md:justify-end md:flex-row  md:w-1/2 ml-0.5">
-                        <FaClock className="mr-0.5  md:text-md hidden"/>
-                        <span className="w-5 md:w-1/2 text-[0.9rem]">Lunedi - Venerdi</span>
-                        <span className="w-5 md:w-1/2  text-[0.9rem]">9:30  - 13:00 | 16:00 - 19:30</span>
+                    <div className="flex flex-col md:justify-end mt-1 md:mt-0 md:ml-5 md:flex-row w-full md:w-1/2 ">
+                        <FaClock className="mr-0.5  md:text-md hidden "/>
+                        <span className=" md:w-1/2 text-[0.9rem]">Lunedi - Venerdi</span>
+                        <span className=" md:w-1/2  text-[0.9rem] ">9:30  - 13:00 | 16:00 - 19:30</span>
                     </div>
                 </div>
 
                 <div className="flex justify-between items-center">
-                    <img src={logo} className="w-5 ml-0" alt="Enrico Erca"/>
+                    <img src={logo} className="w-5 ml-2" alt="Enrico Erca"/>
                     <NavBar/>
                 </div>
                 <h1 className="text-lg md:text-7xl text-center mt-3 mx-1 font-bold">La scelta intelligente per il tuo immobile!</h1>
@@ -208,7 +208,7 @@ set_architectura_interior_design, architectura_interior_design, valueRicerca, se
             <div className="grid grid-cols-1  sm:grid-cols-2 md:grid-cols-3 gap-3">
                 <FadeIn>
                     <Link onClick={()=>setPopupUtenze(true)} className="transition-transform hover:scale-105 mb-5">
-                        < div className="text-center sm:mt-3">
+                        < div className="text-center mt-5">
                             <FaPiggyBank className="text-red-500 text-7xl mx-auto"/>
                             <p style={{color: "#36454F"}} className="font-bold mt-1">Gestione delle Bollette</p>
                             <p style={{color: "#36454F"}} className="w-10 mx-auto">Cerchi un sistema di risparmia delle tue bollette? </p>
